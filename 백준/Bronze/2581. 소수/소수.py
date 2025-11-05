a14a60757a19cd5f8165ed67656c780fabcf2764
Jaehyeon Kim <jkxojkxo@gmail.com>
2025-11-05 19:33:47 +0900

    [Bronze II] Title: 소수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+def is_prime(x):
+    if x < 2:
+        return False
+    for j in range(2, int(x ** 0.5) + 1):
+        if x % j == 0:
+            return False
+    return True             # 위 조건 통과 시 소수
+
+# -------------------- main --------------------
+
+M = int(input())
+N = int(input())   
+
+primes = []
+
+for i in range(M, N + 1):
+    if is_prime(i):
+        primes.append(i)
+
+if not primes:
+    print(-1)
+
+else: 
+    print(sum(primes))
+    print(min(primes))
