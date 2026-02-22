d30a098c92bdff40552c0ebc81ddbb03ba6c8387
Jaehyeon Kim <jkxojkxo@gmail.com>
2026-02-22 22:04:35 +0900

    [Gold IV] Title: 타임머신, Time: 1176 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+n, m = map(int, input().split())
+edges = []
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((a, b, c))
+
+dist = [INF] * (n + 1)
+dist[1] = 0
+
+# N-1번 반복
+for i in range(n - 1):
+    for a, b, c in edges:
+        if dist[a] != INF and dist[b] > dist[a] + c:
+            dist[b] = dist[a] + c
+
+# 음수 사이클 체크
+for a, b, c in edges:
+    if dist[a] != INF and dist[b] > dist[a] + c:
+        print(-1)
+        sys.exit()
+
+for i in range(2, n + 1):
+    if dist[i] == INF:
+        print(-1)
+    else:
+        print(dist[i])
